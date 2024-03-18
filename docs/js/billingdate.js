b715bf290b4c03330a3f4c2c32f1ff53278ca04c
gobravedave <47341076+gobravedave@users.noreply.github.com>
2024-03-19 08:26:19 +1100

    log result

--- docs/js/billingdate.js
+++ docs/js/billingdate.js
@@ -147,10 +147,15 @@ function testCalculateNextBillingDate() {
     testCases.forEach((testCase) => {
         addToResultLog("testcase" +testnbr + `: Start=${testCase.date1} Frequency=${testCase.frequency} As at=${testCase.date2}`);
         const result = calculateNextBillingDate(new Date(testCase.date1), testCase.frequency, new Date(testCase.date2));     
-        addToResultLog("testcase" +testnbr++ + `: Expected= ${testCase.expected.nextBillingDate} Actual= ${result}`);
+        if (testCase.expected.nextBillingDate === result) {
+            addToResultLog("testcase" +testnbr + `: Result= Passed`);
+        } else {
+            addToResultLog("testcase" +testnbr + `: Result= Failed`);
+        }
         console.log("date1:", testCase.date1, "frequency:", testCase.frequency, "Testing date2:", testCase.date2);    
         console.log("Expected:", testCase.expected);
         console.log("Actual:", result);
+        testnbr++;
         });
     }
 
